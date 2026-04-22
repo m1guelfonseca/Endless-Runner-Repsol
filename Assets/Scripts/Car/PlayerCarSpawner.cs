ab5bccce19d60e33b342b8005921e3ad19b69e13
goncalojbsousa <goncalojbsousa@gmail.com>
2026-04-22 17:34:37 +0100

    Select car in main menu

--- Assets/Scripts/Car/PlayerCarSpawner.cs
+++ Assets/Scripts/Car/PlayerCarSpawner.cs
@@ -11,13 +11,40 @@ public class PlayerCarSpawner : MonoBehaviour
     [SerializeField]
     CinemachineCamera cinemachineCamera;
 
+    [Header("Menu")]
+    [SerializeField]
+    bool isMainMenu = false;
+
     //instanciated car
     GameObject instanciatedPlayerCar = null;
+
+    //which car is selected
+    int carIndex = 0;
+
+    //selected car from menu
+    static GameObject selectedCarPrefab = null;
+    
+    Quaternion carRotation = Quaternion.identity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        instanciatedPlayerCar = Instantiate(carPrefabs[0]);
+        if (isMainMenu)
+        {
+            instanciatedPlayerCar = Instantiate(carPrefabs[carIndex].GetComponent<CarHandler>().CarMeshRenderer.gameObject);
+            selectedCarPrefab = carPrefabs[carIndex];
+        }
+        else
+        {
+            if (selectedCarPrefab != null)
+            {
+                instanciatedPlayerCar = Instantiate(selectedCarPrefab);
+            }
+            else
+            {
+                instanciatedPlayerCar = Instantiate(carPrefabs[0]);
+            }
+        }
 
         if (cinemachineCamera != null)
         {
@@ -28,6 +55,46 @@ public class PlayerCarSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMainMenu)
+        {
+            instanciatedPlayerCar.transform.Rotate(new Vector3(0, 20, 0) * Time.deltaTime);
+
+            carRotation = instanciatedPlayerCar.transform.rotation;
+        }
+    }
+
+    void ChangeCar()
+    {
+        Destroy(instanciatedPlayerCar);
+
+        instanciatedPlayerCar = Instantiate(carPrefabs[carIndex].GetComponent<CarHandler>().CarMeshRenderer.gameObject);
+
+        selectedCarPrefab = carPrefabs[carIndex];
+
+        instanciatedPlayerCar.transform.rotation = carRotation;
+    }
+
+    public void OnNextCarClicked()
+    {
+        carIndex++;
+
+        if (carIndex > carPrefabs.Length - 1)
+        {
+            carIndex = 0;
+        }
+
+        ChangeCar();
+    }
+
+    public void OnPreviousCarClicked()
+    {
+        carIndex--;
+
+        if (carIndex < 0)
+        {
+            carIndex = carPrefabs.Length - 1;
+        }
+
+        ChangeCar();
     }
 }
